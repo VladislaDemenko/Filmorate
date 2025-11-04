7656347572dfe5be1adef56e4ff23a7470ec8698
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-11-04 13:36:10 +0300

    FCU-1: Реализованы методы getAllUsers() и createUser() [GET, POST]

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.Main;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    private Long currentId = 1L;
+
+    @GetMapping
+    public Collection<User> getAllUsers() {
+        log.info("Получен запрос на получение всех пользователей. Кол-во на данный момент: {}", users.size());
+        return users.values();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        user.setId(currentId++);
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+            log.info("Для пользователя с id {} установлено имя из логина{}: ", user.getId(), user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("Добавлен новый пользователь: {}", user);
+        return  user;
+    }
+}
