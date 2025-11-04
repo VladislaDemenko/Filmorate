1609a1fea80552af6966d6fccf858eed06e35242
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-11-04 13:53:39 +0300

    FCU-3: Убран ненужный import

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.Main;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
