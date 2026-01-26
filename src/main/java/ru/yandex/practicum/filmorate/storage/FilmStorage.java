27f0277f653163cdab9ecdf9eae248e02bd15b6f
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-01-26 12:38:23 +0300

    FSFS-1: Реализован интерфейс для Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmStorage {
+    List<Film> getAllFilms();
+    Optional<Film> getFilmById(Long id);
+    Film createFilm(Film film);
+    Film updateFilm(Film film);
+    void deleteFilm(Long id);
+}
